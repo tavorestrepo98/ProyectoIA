5c75278d6812e393d71ea8c94bda6ff81940d823
Gustavo Restrepo <gustavo.restrepo@utp.edu.co>
2019-06-15 21:05:47 -0500

    orden de archivos y sonido

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import pygame
+import pyworld as pw
+from gtts import gTTS
+from tempfile import TemporaryFile
+from pygame import mixer
 import spacy
 from spacy.lang.es.examples import sentences
-
 import speech_recognition as sr
+
+
 r = sr.Recognizer()
+ficheromp3 = 'C:\\Users\\tavor\\OneDrive\\Escritorio\\ProyectoIA\\tts.mp3'
 
 nlp = spacy.load('es_core_news_sm')
 
 with sr.Microphone() as source:
     print('Say something')
-    audio = r.listen(source, phrase_time_limit=6)
+    audio = r.listen(source, phrase_time_limit=4)
     print('TIME OVER, THANKS')
 
 try: 
-    text = r.recognize_google(audio, language='es_ES')
+    text = r.recognize_google(audio, language='es-Es')
     print('TEXT ' + text)
 except:
-    print('no funciona')
+    text = 'no funciona'
+    print(text)
 
 doc = nlp(text)
+tts = gTTS(text=text, lang="es")
     
 for token in doc:
-    print(token.text, token.pos_, token.dep_, token.head.text)
+    print(token, token.pos_, token.dep_)
+
+
+
+tts.save(ficheromp3)
+mixer.init()
+mixer.music.load(ficheromp3)
+print("Reproduciendo {0}".format(ficheromp3))
+mixer.music.play()
+while mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+mixer.quit()
 
